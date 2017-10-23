335f9177f4e243d6f72cc14634ae7818d18faefe
Ahzaz <hahzaz@gmail.com>
2017-10-23 22:09:35 +0530

    Updates

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from time import sleep
 import math
 import numpy as np    
 import pygame
-import sys
+import sys  
 
 BIRD_UP = -1
 BIRD_DOWN = 1
@@ -31,7 +31,6 @@ class NeuralNetwork(object):
         z = self.sigmoid(np.matmul(inputs, self.input_to_hidden_weights)) # 1x2 * 2x8 = 1x8
         z = np.matmul(z, self.hidden_to_output_weights)[0] # 1x8 * 8x1 = 1x1        
         return 1 / (1 + math.exp(-z))
-        # return random()
 
     def copy(self):
         nn = NeuralNetwork()
@@ -39,10 +38,10 @@ class NeuralNetwork(object):
         nn.hidden_to_output_weights = np.copy(self.hidden_to_output_weights)
         return nn
 
-    def mate(self, other):        
+    def mate(self, other, w1, w2):        
         nn = NeuralNetwork()
-        nn.input_to_hidden_weights = (self.input_to_hidden_weights + self.input_to_hidden_weights) / 2
-        nn.hidden_to_output_weights = (self.hidden_to_output_weights + self.hidden_to_output_weights) /2
+        nn.input_to_hidden_weights = (w1 * self.input_to_hidden_weights + w2 * other.input_to_hidden_weights) / 2
+        nn.hidden_to_output_weights = (w1 * self.hidden_to_output_weights + w2 * other.hidden_to_output_weights) /2
         return nn
 
     def mutate(self):
@@ -60,6 +59,7 @@ class NeuralNetwork(object):
         return "\nLayer 1 : " + self.input_to_hidden_weights.__repr__() + "\nLayer 2 : " + self.hidden_to_output_weights.__repr__()
 
 
+g = 1
 class Bird(object):
     """docstring for Bird"""
     def __init__(self):
@@ -68,6 +68,8 @@ class Bird(object):
         self.direction = BIRD_UP
         self.alive = True
         self.score = 0
+        self.v = 0
+
 
     def set_pos(self, pos):
         self.current_pos = pos
@@ -100,9 +102,15 @@ class Bird(object):
         height = 600
         return -1 + (1.0 - (-1)) / (height - (-height)) * (d - (-height))
 
-    def move(self, offset, height_limit):
-        self.direction = offset[1]
-        self.current_pos = np.add(self.current_pos, offset)
+    def move(self, offset, height_limit):        
+        if offset[1] < 0:
+            self.direction = BIRD_UP
+            self.v = -2*g
+            # self.current_pos = np.add(self.current_pos, offset)
+        else:
+            self.direction = BIRD_DOWN
+            self.v += g
+        self.current_pos = np.add(self.current_pos, [offset[0], self.v])
         self.current_pos[1] = self.current_pos[1] % height_limit
 
     def x(self):
@@ -117,19 +125,27 @@ class Bird(object):
 
     def mate(self, other):
         newBird = Bird()
-        newBird.nn = self.nn.mate(other.nn)
+        w = float(self.score + other.score)
+        w1 = 0.5
+        w2 = 0.5
+        if w != 0:
+            w1 = self.score / w
+        if w != 0:
+            w2 = other.score / w
+        newBird.nn = self.nn.mate(other.nn, w1, w2)
         return newBird
 
     def mutate(self):
         self.nn.mutate()
         return self
 
-
         
 class Game(object):
     """docstring for Game"""
-    def __init__(self):
+    def __init__(self):        
         super(Game, self).__init__()
+        self.tick_count = 0
+        self.generataion = 0
         self.nr_birds = 10
         self.width  = 800
         self.height = 600
@@ -138,8 +154,11 @@ class Game(object):
         self.reset()
 
     def reset(self):
+        self.generataion += 1
+        if self.generataion % 100 == 0:
+            print self.generataion
         for bird in self.birds:
-            bird.set_pos([20, randint(50, self.height-50)])
+            bird.set_pos([20, self.height/2])
         self.pipes = []
         self.alive_birds = filter(lambda bird: bird.is_alive(), self.birds)
         self.generatePipes()
@@ -150,7 +169,7 @@ class Game(object):
     def generatePipes(self):
         curr_pipe = self.pipe_dist
         # self.pipes = [(curr_pipe, (self.height/2-25, self.height/2+25))]
-        # curr_pipe += self.pipe_dist
+        curr_pipe += self.pipe_dist
         while curr_pipe <= self.width:
             gap = randint(0, self.height)
             self.pipes.append(self.newPipe(curr_pipe))
@@ -161,31 +180,39 @@ class Game(object):
             last_gap = randint(100, self.height-100)
         else:
             last_gap = self.pipes[-1][1][0] + 25
-        gap = randint(last_gap-125,last_gap+125)
-        if gap > self.height-125:
-            gap -= 125
-        if gap < 125:
-            gap += 125
-        
+        diff = 240
+        gap = last_gap
+        while abs(gap - last_gap) < 150:
+            gap = randint(last_gap-diff,last_gap+diff)
+        if gap > self.height-50:
+            gap = self.height-50
+        if gap < 50:
+            gap = 50
+    
         return (x,(gap-25, gap+25))
 
     def debug(self):
         print self.bird.x(), self.bird.y()
 
-    def tick(self):
-        self.alive_birds = filter(lambda bird: bird.is_alive(), self.birds)
+    def tick(self, tick_count):
+        self.alive_birds = filter(lambda bird: bird.is_alive(), self.birds)        
         for bird in self.alive_birds:
-            d = bird.tick(self.pipes[0])
-            print d,
-            if d:
-                bird.move([0,-2], self.height)
-            else:
-                bird.move([0,+3], self.height)
-            if self.collison(bird, self.pipes[0]):
+            direction = 1
+            if bird.tick(self.pipes[0]):                
+                direction = -1            
+            bird.move([0,direction], self.height)
+            
+            if self.collisonWithPipe(bird, self.pipes[0]):
                 bird.kill()
-        print ""
+            elif self.outOfBounds(bird):
+                bird.kill()
+                # bird.score /= 2
+            elif bird.score > 250*100:
+                print bird.nn.__repr__()
+                bird.kill()
+
         if len(self.alive_birds) == 0:
-            # print map(lambda b : b.nn, self.birds)
+            print sorted(map(lambda b : b.score, self.birds), reverse=True)
             self.generateNewGeneratation(self.birds)
             self.reset()
             return
@@ -193,10 +220,15 @@ class Game(object):
 
         for i in range(len(self.pipes)):
             pipe = self.pipes[i]
-            self.pipes[i] = (pipe[0]-1, pipe[1])
+            self.pipes[i] = (pipe[0]-2, pipe[1])
 
         if self.passed(self.alive_birds[0], self.pipes[0]):
+            self.removeFirstPipe()
+
+        # if self.pipes[0][0] < self.pipe_dist:
             self.addNewPipe()
+
+        self.tick_count += 1
 
 
     def generateNewGeneratation(self, old):
@@ -211,15 +243,20 @@ class Game(object):
 
         self.birds = new
 
-    def collison(self, bird, pipe):
-        collison_with_pipe = (bird.x() + self.bird_width/2 > pipe[0] - self.pipe_width/2 and (bird.y() <= pipe[1][0] or bird.y() >= pipe[1][1]))
-        return collison_with_pipe or (bird.y() + self.bird_height/2 > self.height) or (bird.y() - self.bird_height/2 < 0)
+    def outOfBounds(self, bird):
+        return (bird.y() + self.bird_height/2 > self.height) or (bird.y() - self.bird_height/2 < 0)
 
+    def collisonWithPipe(self, bird, pipe):
+        collison_with_pipe = (bird.x() + self.bird_width/2 > pipe[0] - self.pipe_width/2 and (bird.y() <= pipe[1][0] or bird.y() >= pipe[1][1]))
+        return collison_with_pipe
+        
     def passed(self, bird, pipe):
         return bird.x() > pipe[0]
 
-    def addNewPipe(self):
+    def removeFirstPipe(self):
         self.pipes = self.pipes[1:]
+
+    def addNewPipe(self):        
         last_pipe = self.pipes[-1]
         self.pipes.append(self.newPipe(last_pipe[0] + self.pipe_dist))
 
@@ -232,19 +269,21 @@ class GameWindow(object):
         self.game = game
         self.screen = pygame.display.set_mode((self.game.width, self.game.height))
         self.bird_up = pygame.image.load('bird_up.png')
-        self.bird_down = self.bird_up #pygame.image.load('bird_down.png')
+        self.bird_down = pygame.image.load('bird_down.png')
         self.pipe = pygame.image.load('pipe.png')
         #self.birds = [self.bird_up for i in range(game.nr_birds)]
 
     def loop(self, fps):
         loop_delay = 1.0 / fps
+        tick_count = 0
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
-            self.game.tick()
+            self.game.tick(tick_count)
             self.draw()
             # self.debug()
             sleep(loop_delay)
+            tick_count += 1
         
     def draw(self):
         self.screen.fill((0,0,0))        
